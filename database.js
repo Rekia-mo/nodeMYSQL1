1dfbeb804ac2442a9ef44d4a83c4c1b68530fae0
Rekia <rmansourouahchia@gmail.com>
2025-11-20 01:13:09 +0100

    Remove database password from connection pool
    Removed the password from the database connection pool for security reasons.

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2'
 const pool = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: 'r11ee29k06',
+  password: '',
   database: 'notes_app'
 }).promise();
 
