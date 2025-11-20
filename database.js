22058a6e89bfb336acfe62c2d7bf1b22e4072545
Rekia Mansour Ouahchia <rmansourouahchia@gmail.com>
2025-11-20 01:05:57 +0100

    first commit

---
+++ database.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2'
+
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'r11ee29k06',
+  database: 'notes_app'
+}).promise();
+
+async function getNotes(){
+  const [rows] = await pool.query("SELECT * FROM notes");
+  return rows;
+}
+console.log( await getNotes());
